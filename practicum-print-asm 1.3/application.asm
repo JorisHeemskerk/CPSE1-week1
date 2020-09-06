59179b906c9588842e1e13b03c6c00e3e3cf90b7
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2020-09-06 16:18:50 +0200

    initial commit

---
+++ practicum-print-asm 1.3/application.asm
@@ -0,0 +1,13 @@
+    .cpu cortex-m0
+    .text
+    .align 2
+    .global application
+
+text:
+    .asciz "Hello world, the ANSWER is 42! @[]`{}~\n"
+application:
+    push { lr }
+    ldr r0, =text
+    bl print_asciz
+    pop { pc }
+
